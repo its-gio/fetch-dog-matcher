2e501d0931e7d9e89a5d2896e79efd5711978681
its-gio <9650600+its-gio@users.noreply.github.com>
2025-02-11 15:27:56 -0800

    Adding video background to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-
 export default function Home() {
   return (
-    <div className="">
-      <main className="">Main</main>
-      <footer className="">Footer</footer>
-    </div>
+    <main id="homepage">
+      <div className="hero relative h-screen flex text-center justify-center items-center overflow-hidden">
+        <div>
+          <h1>Find your Best Friend!</h1>
+          <h4>For those tired of chasing their tails</h4>
+        </div>
+
+        <video
+          autoPlay
+          loop
+          muted
+          className="hero-bg-video z-[-1] object-cover absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-full h-full"
+        >
+          <source src="/video/homepage-background.mp4" />
+        </video>
+      </div>
+    </main>
   );
 }
